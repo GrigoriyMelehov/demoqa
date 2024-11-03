103b6f0db4c8029d7c33ed2280ff9525cba0c9d8
Grinja1984@outlook.com <Grigory_Melehov>
2024-11-03 10:58:26 +0300

    modified right

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -2,12 +2,16 @@ from pages.demoqa import DemoQa
 from pages.elements_page import ElementsPage
 import time
 
-def test_check_text_elements(browser):
+def test_check_text_demoqa_footer(browser):
     demo_qa_page = DemoQa(browser)
-    elements_page = ElementsPage(browser)
     demo_qa_page.visit()
     time.sleep(5)
     assert demo_qa_page.footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+def test_check_elements_pega_element(browser):
+    demo_qa_page = DemoQa(browser)
+    elements_page = ElementsPage(browser)
+    demo_qa_page.visit()
     time.sleep(5)
     demo_qa_page.btn_elements.click()
     time.sleep(5)
